f8fc6928919297522e3a19985795041a7d6e032a
paulik123 <paul@b-r-g.ro>
2025-10-23 16:59:12 +0300

    simplified args and added --latestlocal option to skip dumping the remote database

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ## How to use
 1. Copy `exampleenv` in ./envs/<projectname>
 2. Edit the new `env` with your credentials.
-3. Run the project: `python3 sync.py -p <projectname>` or `uv run sync.py -p <projectname>`
+3. Run the project: `python3 sync.py <projectname>` or `uv run sync.py <projectname>`
 
 
 ##### Bash alias
--- sync.py
+++ sync.py
@@ -5,6 +5,12 @@ from dotenv import dotenv_values
 from pathvalidate import validate_filename
 import subprocess
 from datetime import datetime
+from pathlib import Path 
+
+####### UTILS #######
+def now():
+	return datetime.now().isoformat(sep=" ", timespec="seconds")
+
 
 root_status = subprocess.check_output("sudo whoami", shell=True)
 if 'root' not in str(root_status):
@@ -12,10 +18,12 @@ if 'root' not in str(root_status):
 	exit()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--project", type=str, required=True)
+parser.add_argument("project", nargs=1)
+parser.add_argument("-ll", "--latestlocal", action='store_true', help="Skips dumping the remote database and uses the latest local version.")
 
 args = parser.parse_args()
-project = args.project
+project = args.project[0]
+print(project)
 
 try:
 	validate_filename(project)
@@ -54,17 +62,30 @@ if 'connection_open | 1' in str(connection_status):
 
 print(f"======= Syncing {project} =======")
 
-print("\n======= Droping local database and creating a new one =======")
+print(f"[{now()}] Droping local database and creating a new one")
 os.system(f'''sudo -u postgres psql -c "drop database if exists {config["LOCAL_DATABASE"]};" -c "create database {config["LOCAL_DATABASE"]};" -c "alter database {config["LOCAL_DATABASE"]} owner to {config["LOCAL_USERNAME"]};"''')
-print("=== Done ===")
+print(f"[{now()}] Done")
 
+print()
+if not args.latestlocal:
+	print(f"[{now()}] Dumping remote database to ./dumps")
+	filename = f"{project}_{datetime.now().strftime('%Y%m%d%H%M%S')}.sql"
+	os.system(f"""pg_dump --no-owner --dbname=postgresql://{config["REMOTE_USERNAME"]}:{config["REMOTE_PASSWORD"]}@{config["REMOTE_HOST"]}:{config["REMOTE_PORT"]}/{config["REMOTE_DATABASE"]} --format=c  > ./dumps/{filename}""")
+	print(f"[{now()}] Saved dump to ./dumps/{filename}")
+	print(f"[{now()}] Done")
+else:
+	dumps_path = p=Path('./dumps/')
+	try:
+		filename = os.path.basename(max([fn for fn in p.glob('*') if project in str(fn)], key=lambda f: f.stat().st_mtime))
+	except:
+		print(f"⛔ ERROR: Could not find any local dump for project {project}")
+		exit()
+	print(f"[{now()}] Using latest local dump {filename}")
 
-print("\n======= Dumping remote database to ./dumps =======")
-filename = f"{project}_{datetime.now().strftime('%Y%m%d%H%M%S')}.sql"
-os.system(f"""pg_dump --no-owner --dbname=postgresql://{config["REMOTE_USERNAME"]}:{config["REMOTE_PASSWORD"]}@{config["REMOTE_HOST"]}:{config["REMOTE_PORT"]}/{config["REMOTE_DATABASE"]} --format=c  > ./dumps/{filename}""")
-print(f"Saved dump to ./dumps/{filename}")
-print("=== Done ===")
-
-print(f"\n======= Restoring local database [{config['LOCAL_DATABASE']}] =======")
+print()
+print(f"[{now()}] Restoring local database [{config['LOCAL_DATABASE']}]")
 os.system(f'''pg_restore --no-owner --dbname 'postgresql://{config["LOCAL_USERNAME"]}:{config["LOCAL_PASSWORD"]}@localhost:{config["LOCAL_PORT"]}/{config["LOCAL_DATABASE"]}' ./dumps/{filename}''')
-print("=== Done ===")
+print(f"[{now()}] Done")
+
+
+
